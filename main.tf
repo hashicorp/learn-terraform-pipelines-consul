0eb5a839c8f028b45ce5be3862c8d027f11597ad
Rachel Sharp <rachel@hashicorp.com>
2021-02-02 18:09:39 -0500

    update cluster

--- main.tf
+++ main.tf
@@ -32,21 +32,34 @@ data "terraform_remote_state" "cluster" {
   }
 }
 
-provider "kubernetes" {
-
-  load_config_file       = false
-  host                   = data.terraform_remote_state.cluster.outputs.host
-  username               = data.terraform_remote_state.cluster.outputs.username
-  password               = data.terraform_remote_state.cluster.outputs.password
-  cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
-}
 
 provider "helm" {
   kubernetes {
-    load_config_file       = false
     host                   = data.terraform_remote_state.cluster.outputs.host
-    username               = data.terraform_remote_state.cluster.outputs.username
-    password               = data.terraform_remote_state.cluster.outputs.password
-    cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.outputs.cluster_ca_certificate)
   }
+}
+
+
+# Retrieve GKE cluster information
+provider "google" {
+  project = data.terraform_remote_state.cluster.outputs.project_id
+  region  = data.terraform_remote_state.cluster.outputs.region
+}
+
+# Configure kubernetes provider with Oauth2 access token.
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
+# This fetches a new token, which will expire in 1 hour.
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = data.terraform_remote_state.cluster.outputs.kubernetes_cluster_name
+  location = data.terraform_remote_state.cluster.outputs.region
+}
+
+provider "kubernetes" {
+  host = data.terraform_remote_state.cluster.outputs.kubernetes_cluster_host
+
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
 }
