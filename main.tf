d30122f3b37e7d1189a073854e9c9e6baaf52973
Rachel Sharp <rachel@hashicorp.com>
2021-02-02 18:22:06 -0500

    kube

--- main.tf
+++ main.tf
@@ -41,8 +41,6 @@ data "google_container_cluster" "my_cluster" {
 
 
 provider "kubernetes" {
-  load_config_file = false
-
   host  = data.terraform_remote_state.cluster.outputs.host
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
