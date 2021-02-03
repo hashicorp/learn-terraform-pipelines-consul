536afd6da09810ead2f7f766f2c4e6914db34231
Rachel Sharp <rachel@hashicorp.com>
2021-02-03 16:52:39 -0500

    kubernetes auth

--- main.tf
+++ main.tf
@@ -34,20 +34,32 @@ data "terraform_remote_state" "cluster" {
 
 
 
+# Retrieve GKE cluster information
+provider "google" {
+  project = data.terraform_remote_state.cluster.outputs.project_id
+  region  = data.terraform_remote_state.cluster.outputs.region
+}
+
+# Configure kubernetes provider with Oauth2 access token.
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
+# This fetches a new token, which will expire in 1 hour.
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = data.terraform_remote_state.cluster.outputs.kubernetes_cluster_name
+  location = data.terraform_remote_state.cluster.outputs.region
+}
 
 provider "kubernetes" {
-  host                   = data.terraform_remote_state.cluster.outputs.host
-  username               = data.terraform_remote_state.cluster.outputs.username
-  password               = data.terraform_remote_state.cluster.outputs.password
-  cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
+  host = data.terraform_remote_state.cluster.outputs.kubernetes_cluster_host
+
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.terraform_remote_state.cluster.outputs.host
-    username               = data.terraform_remote_state.cluster.outputs.username
-    password               = data.terraform_remote_state.cluster.outputs.password
-    cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
-
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
   }
 }
