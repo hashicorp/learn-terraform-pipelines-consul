85802c9f82ae60b66c4aed067b1720a8b90d131a
Rachel Sharp <rachel@hashicorp.com>
2021-02-03 19:46:11 -0500

    outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,16 @@ output "namespace" {
 output "release_name" {
   value = var.release_name
 }
+
+output "project_id" {
+  value = data.google_client_config.default.project
+}
+
+output "region" {
+  value = data.google_client_config.default.region
+}
+
+
+output "cluster" {
+  value = google_container_cluster.engineering.name
+}
