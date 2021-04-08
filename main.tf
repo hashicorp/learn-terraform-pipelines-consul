21a6ea7d025270cca1d86f6abc56cab3be188261
Robin Norwood <rnorwood@hashicorp.com>
2021-04-08 10:31:29 -0500

    Fix workspace name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "firstName-lastInitial-vault"
+      name = "firstName-lastInitial-consul"
     }
   }
 
