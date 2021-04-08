45f4f051a8148d8aa4771081acfd857fa99cd297
Robin Norwood <rnorwood@hashicorp.com>
2021-04-08 10:47:58 -0500

    Change cluster_id output name

--- main.tf
+++ main.tf
@@ -40,11 +40,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks_cluster.outputs.cluster
+  name = data.terraform_remote_state.eks_cluster.outputs.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks_cluster.outputs.cluster
+  name = data.terraform_remote_state.eks_cluster.outputs.cluster_id
 }
 
 provider "kubernetes" {
