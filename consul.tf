836089fc45f61ec7596676d7612b0d83f4502b18
Brian McClain <brianmmcclain@gmail.com>
2023-02-02 11:43:17 -0500

    Remove enable_consul_and_vault variable

--- consul.tf
+++ consul.tf
@@ -1,5 +1,4 @@
 resource "helm_release" "consul" {
-  count      = data.tfe_outputs.cluster.values.enable_consul_and_vault ? 1 : 0
   depends_on = [kubernetes_namespace.secrets]
   name       = "${var.release_name}-consul"
   repository = "https://helm.releases.hashicorp.com"
--- namespace.tf
+++ namespace.tf
@@ -3,7 +3,6 @@
 # https://github.com/terraform-providers/terraform-provider-helm/issues/427
 
 resource "kubernetes_namespace" "secrets" {
-  count = data.tfe_outputs.cluster.values.enable_consul_and_vault ? 1 : 0
   metadata {
     annotations = {
       name = var.namespace
